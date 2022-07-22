bcc54c821b204e89a836c3c870c51c63cbf4e49a
Raisan <ytpc26ms@outlook.jp>
2022-07-22 11:09:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-docker-compose run --rm outline yarn sequelize db:create --env=production-ssl-disabled
+docker compose run --rm outline yarn sequelize db:create --env=production-ssl-disabled
 
-docker-compose run --rm outline yarn sequelize db:migrate --env=production-ssl-disabled
+docker compose run --rm outline yarn sequelize db:migrate --env=production-ssl-disabled
